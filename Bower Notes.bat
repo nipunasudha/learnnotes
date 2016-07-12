3c76ff2f247f35c332898b40aeb5b1c390ada578
nipunasudha <nipuna.14@cse.mrt.ac.lk>
2016-07-12 01:49:04 -0700

    my bower note is complete 	modified:   Bower Notes.bat

--- Bower Notes.bat
+++ Bower Notes.bat
@@ -1,11 +1,25 @@
+BOWER NOTES
+
 installing bower--------------------
-1)install npm, git
-2)npm install -g bower
+    1)install npm, git
+    2)npm install -g bower
 
 
 installing a package!---------------
-1)go to the project dir
-2)bower install jquery
-
+    1)go to the project dir
+    2)bower install jquery
+    3)to save to the JSON while installing, "bower install jquery -S"
 installing with a version----------
-1)bower install jquery#1.2.2
+    1)bower install jquery#1.2.2
+
+listing all paths------------------
+    bower list --paths
+
+uninstalling------------------------
+    bower uninstall jquery
+
+creating bower.js Filter
+      bower init
+
+Installing packages using bower.JSON
+      bower install
